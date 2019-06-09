271df6b49a316d6f2b3f2dce7f79063d439afa1d
James Dennis <44235413+JamesDennis1995@users.noreply.github.com>
2019-06-09 13:16:15 +0100

    Update Login.aspx.cs

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -12,6 +12,7 @@ namespace WebApplication3
 {
     public partial class Login : System.Web.UI.Page
     {
+        //Declare a hashed password variable.
         static string hashedPassword;
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -20,6 +21,7 @@ namespace WebApplication3
 
         protected void CustomerLogin_Click(object sender, EventArgs e)
         {
+            //Declare SQL connection and command variables.
             SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
             SqlCommand cmd = new SqlCommand();
             SqlCommand cmd2 = new SqlCommand();
@@ -30,6 +32,7 @@ namespace WebApplication3
             cmd2.CommandText = "SELECT ID, FirstName FROM Customer WHERE Email = @email";
             cmd2.Parameters.Add("@email", SqlDbType.NChar).Value = Email.Text;
             conn.Open();
+            //Check if any email address in the database matches the one entered. If no rows are returned, generate an error message. Otherwise, proceed.
             SqlDataReader reader = cmd.ExecuteReader();
             if (reader.HasRows == false)
             {
@@ -39,9 +42,11 @@ namespace WebApplication3
             {
                 while (reader.Read())
                 {
+                    //Sets the hashed password variable.
                     hashedPassword = reader["Password"].ToString();
                 }
                 reader.Close();
+                //Checks the hashed password. If it isn't correct, generate an error message. If it is, set session ID and message and redirect to the User page.
                 bool correct = Salt.Verify(Password.Text, hashedPassword);
                 if (correct == false)
                 {
